4b42ffe9e05c026e549a0825b08fa6564a6a4df0
Alaanor <max.bonvin@gmail.com>
2020-01-30 00:42:21 +0100

    Magic number + Method to validate it

---
+++ Blister.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Blister {
+    class PlaylistLib
+    {
+        public string $magicNumber = "Blist.v2";
+
+        /**
+         * Check if the file has the magic number
+         *
+         * @param resource $handler
+         * @return bool
+         */
+        public function HasMagicNumber ($handler): bool {
+            fseek($handler, 0);
+            return fread($handler, strlen($this->magicNumber)) === $this->magicNumber;
+        }
+    }
+}
