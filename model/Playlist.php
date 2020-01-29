a6ed246823f4612aa33b0da07071776f153486ec
Alaanor <max.bonvin@gmail.com>
2020-01-30 00:40:38 +0100

    Playlist model

--- model/Playlist.php
+++ model/Playlist.php
@@ -1,39 +1,41 @@
 <?php
 
-class Playlist
-{
-    /**
-     * @var string $title
-     *
-     * The title of the playlist
-     */
-    public string $title;
+namespace Blister {
+    class Playlist
+    {
+        /**
+         * @var string $title
+         *
+         * The title of the playlist
+         */
+        public string $title;
 
-    /**
-     * @var string $author
-     *
-     * The author of the playlist
-     */
-    public string $author;
+        /**
+         * @var string $author
+         *
+         * The author of the playlist
+         */
+        public string $author;
 
-    /**
-     * @var string|null $description
-     *
-     * The description of the playlist, optional
-     */
-    public ?string $description;
+        /**
+         * @var string|null $description
+         *
+         * The description of the playlist, optional
+         */
+        public ?string $description;
 
-    /**
-     * @var string|null $cover
-     *
-     * Binary data of the cover, can be PNG/JPEG, should be 1:1
-     */
-    public ?string $cover;
+        /**
+         * @var string|null $cover
+         *
+         * Binary data of the cover, can be PNG/JPEG, should be 1:1
+         */
+        public ?string $cover;
 
-    /**
-     * @var Beatmap[] $maps
-     *
-     * The beatmaps of the playlist
-     */
-    public Array $maps;
+        /**
+         * @var Beatmap[] $maps
+         *
+         * The beatmaps of the playlist
+         */
+        public Array $maps;
+    }
 }
